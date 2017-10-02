decd9071e6faa9ea8f95bb83b9a80d3a0f1c88f3
Cheikh Seck <Cheikh@pc164.home>
2017-10-02 08:37:16 +0000

    init

---
+++ form.go
@@ -0,0 +1,197 @@
+package form
+
+import (
+	"time"
+	"github.com/asaskevich/govalidator"
+	"crypto/aes"
+	"crypto/cipher"
+	"crypto/rand"
+	"encoding/base64"
+	"gopkg.in/mgo.v2/bson"
+	"github.com/cheikhshift/gos/core"
+	"fmt"
+	"net/http"
+	"encoding/json"
+	"io"
+	"strings"
+	"reflect"
+	"os"
+)
+
+type File string 
+type Paragraph string 
+type Date string
+type Select string
+type SelectMult Select
+type Radio Select
+type Email string
+//select opts,placeholder,title
+const (
+        MB = 1 << 20
+      
+)
+
+var (
+	InputClass string
+	ButtonClass string
+	FormKey  = "a very very very very secret key"
+	MaxSize = 10 //Mb
+)
+
+func GetSel(tagstr string) string {
+	strs := strings.Split(tagstr,"\",")
+	for _,v := range strs {
+		if strings.Contains(v,"select") {
+			splm := strings.Split(v,":")
+			return strings.Replace(splm[1], "\"","", -1)
+		}
+	}
+
+	return ""
+}
+
+func GetPl(tagstr string) string {
+	strs := strings.Split(tagstr,"\",")
+	for _,v := range strs {
+		if strings.Contains(v,"placeholder") {
+			splm := strings.Split(v,":")
+			return strings.Replace(splm[1], "\"","", -1)
+		}
+	}
+
+	return ""
+}
+
+func Path(pathtoformat File) string {
+	return fmt.Sprintf("./uploads/%s", pathtoformat )
+}
+
+
+func Form(r *http.Request, i interface{}) error {
+	//convert form and validate 
+	var err error
+	v := reflect.ValueOf(i).Elem()
+			//t := reflect.TypeOf(item)
+			bso :=  bson.M{"__elem" : "form"}
+
+			for i := 0; i < v.NumField(); i++ {
+				field := v.Type().Field(i)
+				
+					fieldtype := strings.ToLower(field.Type.String())
+				
+					
+					if strings.Contains(fieldtype, "file"){
+						fmt.Println("add file!")
+					file, handler, err := r.FormFile(field.Name)
+			        if err != nil {        
+			            return err
+			        }
+	      
+	 
+	      		fid := fmt.Sprintf("%s-%s", core.NewLen(15),handler.Filename )
+			        f, err := os.OpenFile(fmt.Sprintf("./uploads/%s", fid ), os.O_WRONLY|os.O_CREATE, 0700)
+			        if err != nil {
+			           
+			            return err
+			        }
+			        
+			 
+			        io.Copy(f, file)
+			        f.Close()
+			        bso[field.Name] = fid
+			        file.Close()
+				} else if strings.Contains(fieldtype, "bool"){
+					bso[field.Name] = strings.Contains( r.FormValue(field.Name), "on")
+				}  else  {
+					bso[field.Name] = r.FormValue(field.Name)
+				
+				}
+			
+			}
+
+	data,_ := json.Marshal(&bso)
+	json.Unmarshal([]byte(data), i)
+
+	_, err = govalidator.ValidateStruct(i)
+	if err != nil {
+			return err
+	}
+
+	return nil
+	//ValidateStruct
+}
+
+func SetKey(key string) {
+	FormKey = key
+}
+
+func ToBson(data string) bson.M {
+	res := bson.M{}
+    json.Unmarshal([]byte(data), &res)
+    return res
+}
+
+func ValidateRequest(r *http.Request, usertoken string) bool {
+	token := Decrypt([]byte(FormKey),r.FormValue("formtoken"))
+	return strings.Contains(token, usertoken ) && strings.Contains(token, r.URL.Path )
+}
+
+func GenerateToken(url string, usertoken string) string {
+	return Encrypt([]byte(FormKey), fmt.Sprintf("%s@%s@%s",url, time.Now().String() , usertoken) )
+}
+
+func Encrypt(key []byte, text string) string {
+	// key := []byte(keyText)
+	plaintext := []byte(text)
+
+	block, err := aes.NewCipher(key)
+	if err != nil {
+		return err.Error()
+	}
+
+	// The IV needs to be unique, but not secure. Therefore it's common to
+	// include it at the beginning of the ciphertext.
+	ciphertext := make([]byte, aes.BlockSize+len(plaintext))
+	iv := ciphertext[:aes.BlockSize]
+	if _, err := io.ReadFull(rand.Reader, iv); err != nil {
+		return err.Error()
+	}
+
+	stream := cipher.NewCFBEncrypter(block, iv)
+	stream.XORKeyStream(ciphertext[aes.BlockSize:], plaintext)
+
+	// convert to base64
+	return base64.URLEncoding.EncodeToString(ciphertext)
+}
+
+//func GetFile
+
+
+//func DeleteFile add support to html file filters : file:"mimeType"
+
+
+
+// decrypt from base64 to decrypted string
+func Decrypt(key []byte, cryptoText string) string {
+	ciphertext, _ := base64.URLEncoding.DecodeString(cryptoText)
+
+	block, err := aes.NewCipher(key)
+	if err != nil {
+		return err.Error()
+	}
+
+	// The IV needs to be unique, but not secure. Therefore it's common to
+	// include it at the beginning of the ciphertext.
+	if len(ciphertext) < aes.BlockSize {
+		return "ciphertext too short"
+	}
+	iv := ciphertext[:aes.BlockSize]
+	ciphertext = ciphertext[aes.BlockSize:]
+
+	stream := cipher.NewCFBDecrypter(block, iv)
+
+	// XORKeyStream can work in-place if the two arguments are the same.
+	stream.XORKeyStream(ciphertext, ciphertext)
+
+	return fmt.Sprintf("%s", ciphertext)
+}
---
+++ readme.md
@@ -0,0 +1,153 @@
+# Go-Form
+Create HTML forms with your structs and interfaces.
+
+### Requirements
+- [Go Server](http://golangserver.com)
+
+
+### Install and import
+Add this tag within your `.gxml` file.
+
+	<import src="github.com/cheikhshift/form/gos.gxml" />	
+
+
+### Configure
+Set max upload size :	
+
+	netform.MaxSize = 20 //mb
+Set form token key :
+	
+	netform.FormKey = "a very very very very secret key"
+Set input field class :
+	
+	netform.InputClass = ""
+Set submit button class :
+	
+	netform.ButtonClass = ""
+### How to use
+
+Build a new form within a Golang server template:
+							
+	{{ Build $arg1 $arg2 $arg3 $arg4 .Session }}
+			
+Argument information :
+
+- `$arg1` : Interface to build form with. Submit a variable with data to prepopulate form.
+- `$arg2` :  Target URL to submit form to.
+- `$arg3` : Method of form submission.
+- `$arg4` : Call to action of form button.
+- `.Session` :  Current user session. Must be passed to ensure secure communication.
+		
+Build within `<end>` tag :
+
+	net_Build(param1 interface{}, param2 string, param3 string, param4 string, param5 *sessions.Session) string
+
+Parameter information :
+
+- `param1` : Interface to build form with. Submit a variable with data to prepopulate form.
+- `param2` :  Target URL to submit form to.
+- `param3` : Method of form submission.
+- `param4` : Call to action of form button.
+- `param5` :  Current user session. Must be passed to ensure secure communication.
+
+### Server side validation
+Please visit the GoValidator page for valuable tag information. [here](https://github.com/asaskevich/govalidator)
+
+### How to process data.
+Within your `<end>` tag use the following function to validate and convert the post body to the specified interface.
+
+	var sampleform SampleForm
+    err := netform.Form(r, &sampleform)
+
+### Field types
+List of field types with associated tag behavior.
+
+
+#### 1. string
+Display text input box.
+
+Tag properties :
+- title : title of field.
+- Placeholder : placeholder of field.
+
+#### 2. int | float (any number)
+Display number input box.
+
+Tag properties :
+- title : title of field.
+- Placeholder : placeholder of field.
+
+#### 3. Bool
+Display Checkbox.
+
+Tag properties :
+- title : text blurb right of checkbox.
+
+#### 4. File
+Display file upload box. Use this field property with function `netform.Path`to get local filesystem path.
+
+Tag properties :
+- Title : title of field.
+- file : Mimetype of file to upload. 
+
+#### 5. Paragraph
+Display text area.
+
+Tag properties :
+- Title : title of field.
+- Placeholder : placeholder of field.
+
+#### 6. Date
+Display date input.
+
+Tag properties :
+- Title : title of field.
+- Placeholder : placeholder of field.
+
+#### 7. Select
+Display dropdown list.
+
+Tag properties :
+- Title : title of field.
+- Placeholder : Prompt left of field.
+- select : comma delimited choices of field.
+
+#### 8. SelectMult
+Display dropdown list with multiple selection support.
+
+Tag properties :
+- Title : title of field.
+- Placeholder : Prompt left of field.
+- select : comma delimited choices of field.
+
+#### 9. Radio
+Display radio input.
+
+Tag properties :
+- Title : title of field.
+- select : comma delimited choices of field.
+
+#### 10. Email
+Display email input.
+
+Tag properties :
+- Title : title of field.
+- Placeholder : placeholder of field.
+
+
+### Samples
+
+Sample of GoS `<struct/>` with form tags set : 
+
+	<struct name="SampleForm">
+			TestField string `title:"Hi world!",valid:"unique",placeholder:"Testfield prompt"`
+			Count int `placeholder:"Count"`
+			Name string `valid:"required",title:"Input title"`
+			FieldTwo netform.Radio `title:"Enter Email",valid:"email,unique,required",select:"blue,orange,red,green"`
+			FieldF netform.Select `placeholder:"Prompt?",valid:"email,unique,required",select:"blue,orange,red,green"`
+			Created netform.Date
+			Text netform.Paragraph 	`title:"Enter a description."`
+			Photo netform.File 	`file:"image/*"`
+			Emal netform.Email
+			Terms bool	`title:"Accept terms of use."`
+	</struct>
