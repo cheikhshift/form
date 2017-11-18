e7d2ab04b1db1cdfc62c4149c2e99a6759b2b42c
Cheikh Seck <Cheikh@pc164.home>
2017-11-18 08:02:49 +0000

    add : angular and json post body support.

--- form.go
+++ form.go
@@ -1,52 +1,54 @@
 package form
 
 import (
-	"time"
-	"github.com/asaskevich/govalidator"
 	"crypto/aes"
 	"crypto/cipher"
 	"crypto/rand"
-	"encoding/base64"
 	"crypto/sha512"
-	"gopkg.in/mgo.v2/bson"
-	"github.com/cheikhshift/gos/core"
-	"fmt"
-	"net/http"
+	"encoding/base64"
 	"encoding/json"
+	"fmt"
+	"github.com/asaskevich/govalidator"
+	"github.com/cheikhshift/gos/core"
+	"gopkg.in/mgo.v2/bson"
 	"io"
-	"strings"
-	"reflect"
+	"net/http"
 	"os"
+	"reflect"
 	"strconv"
+	"strings"
+	"time"
 )
 
-type File string 
-type Paragraph string 
+type File string
+type Paragraph string
 type Date string
 type Select string
 type SelectMult Select
 type Radio Select
 type Email string
 type Password []byte
+type Button string
+
+
 //select opts,placeholder,title
 const (
-        MB = 1 << 20
-      
+	MB = 1 << 20
 )
 
 var (
-	InputClass string
+	InputClass  string
 	ButtonClass string
-	FormKey  = "a very very very very secret key"
-	MaxSize = 10 //Mb
+	FormKey     = "a very very very very secret key"
+	MaxSize     = 10 //Mb
 )
 
 func GetSel(tagstr string) string {
-	strs := strings.Split(tagstr,"\",")
-	for _,v := range strs {
-		if strings.Contains(v,"select") {
-			splm := strings.Split(v,":")
-			return strings.Replace(splm[1], "\"","", -1)
+	strs := strings.Split(tagstr, "\",")
+	for _, v := range strs {
+		if strings.Contains(v, "select") {
+			splm := strings.Split(v, ":")
+			return strings.Replace(splm[1], "\"", "", -1)
 		}
 	}
 
@@ -54,11 +56,11 @@ func GetSel(tagstr string) string {
 }
 
 func GetPl(tagstr string) string {
-	strs := strings.Split(tagstr,"\",")
-	for _,v := range strs {
-		if strings.Contains(v,"placeholder") {
-			splm := strings.Split(v,":")
-			return strings.Replace(splm[1], "\"","", -1)
+	strs := strings.Split(tagstr, "\",")
+	for _, v := range strs {
+		if strings.Contains(v, "placeholder") {
+			splm := strings.Split(v, ":")
+			return strings.Replace(splm[1], "\"", "", -1)
 		}
 	}
 
@@ -66,67 +68,78 @@ func GetPl(tagstr string) string {
 }
 
 func Hash(input string) []byte {
-	return sha512.New512_256().Sum([]byte(input ))
+	return sha512.New512_256().Sum([]byte(input))
 }
-	
 
 func Path(pathtoformat File) string {
-	return fmt.Sprintf("./uploads/%s", pathtoformat )
+	return fmt.Sprintf("./uploads/%s", pathtoformat)
 }
 
-
 func Form(r *http.Request, i interface{}) error {
-	//convert form and validate 
+	//convert form and validate
 	var err error
 	v := reflect.ValueOf(i).Elem()
-			//t := reflect.TypeOf(item)
-			bso :=  bson.M{"__elem" : "form"}
+	//t := reflect.TypeOf(item)
+	if strings.Contains(r.Header.Get("content-type"), "/json" ) {
 
-			for i := 0; i < v.NumField(); i++ {
-				field := v.Type().Field(i)
-				fieldtype := strings.ToLower(field.Type.String())				
-					if strings.Contains(fieldtype, "file"){			
-						file, handler, _ := r.FormFile(field.Name)
+		decoder := json.NewDecoder(r.Body)
+    	err := decoder.Decode(i)
+    	if err != nil {
+       		return err
+    	}
+   
+		_, err = govalidator.ValidateStruct(i)
+		if err != nil {
+			return err
+		}
+	} else {
 
-						if handler != nil {
-		      			fid := fmt.Sprintf("%s-%s", core.NewLen(15),handler.Filename )
-				        f, err := os.OpenFile(fmt.Sprintf("./uploads/%s", fid ), os.O_WRONLY|os.O_CREATE, 0700)
-				        if err != nil {
-				           
-				            return err
-				        }
-				        
-				 
-				        io.Copy(f, file)
-				        f.Close()
-				        bso[field.Name] = fid
-				        file.Close()
-			    	}
-				} else if strings.Contains(fieldtype, "bool"){
-					bso[field.Name] = strings.Contains( r.FormValue(field.Name), "on")
-				} else if strings.Contains(fieldtype, "password"){
-					bso[field.Name] = Hash( r.FormValue(field.Name) )
-				}  else if strings.Contains(fieldtype, "int")  {
-					i, _ := strconv.Atoi(r.FormValue(field.Name))
-					bso[field.Name] = i
-				} else if strings.Contains(fieldtype, "float") {
-					f, _ := strconv.ParseFloat(r.FormValue(field.Name), 64)
-					bso[field.Name] = f
-				} else  {
-					bso[field.Name] = r.FormValue(field.Name)
-				
+	bso := bson.M{"__elem": "form"}
+
+	for i := 0; i < v.NumField(); i++ {
+		field := v.Type().Field(i)
+		fieldtype := strings.ToLower(field.Type.String())
+		if strings.Contains(fieldtype, "file") {
+			file, handler, _ := r.FormFile(field.Name)
+
+			if handler != nil {
+				fid := fmt.Sprintf("%s-%s", core.NewLen(15), handler.Filename)
+				f, err := os.OpenFile(fmt.Sprintf("./uploads/%s", fid), os.O_WRONLY|os.O_CREATE, 0700)
+				if err != nil {
+
+					return err
 				}
-			
-			}
 
-	data,_ := json.Marshal(&bso)
+				io.Copy(f, file)
+				f.Close()
+				bso[field.Name] = fid
+				file.Close()
+			}
+		} else if strings.Contains(fieldtype, "bool") {
+			bso[field.Name] = strings.Contains(r.FormValue(field.Name), "on")
+		} else if strings.Contains(fieldtype, "password") {
+			bso[field.Name] = Hash(r.FormValue(field.Name))
+		} else if strings.Contains(fieldtype, "int") {
+			i, _ := strconv.Atoi(r.FormValue(field.Name))
+			bso[field.Name] = i
+		} else if strings.Contains(fieldtype, "float") {
+			f, _ := strconv.ParseFloat(r.FormValue(field.Name), 64)
+			bso[field.Name] = f
+		} else {
+			bso[field.Name] = r.FormValue(field.Name)
+
+		}
+
+	}
+
+	data, _ := json.Marshal(&bso)
 	json.Unmarshal([]byte(data), i)
 
 	_, err = govalidator.ValidateStruct(i)
 	if err != nil {
-			return err
+		return err
 	}
-
+	}
 	return nil
 	//ValidateStruct
 }
@@ -137,17 +150,30 @@ func SetKey(key string) {
 
 func ToBson(data string) bson.M {
 	res := bson.M{}
-    json.Unmarshal([]byte(data), &res)
-    return res
+	json.Unmarshal([]byte(data), &res)
+	return res
 }
 
 func ValidateRequest(r *http.Request, usertoken string) bool {
-	token := Decrypt([]byte(FormKey),r.FormValue("formtoken"))
-	return strings.Contains(token, usertoken ) && strings.Contains(token, r.URL.Path )
+	var token string
+	if strings.Contains(r.Header.Get("content-type"), "/json" ) {
+		decoder := json.NewDecoder(r.Body)
+		var i bson.M
+    	err := decoder.Decode(i)
+    	if err != nil {
+       		return false
+    	}
+
+    	token = Decrypt([]byte(FormKey), i["formtoken"].(string) )
+
+    } else {
+		token = Decrypt([]byte(FormKey), r.FormValue("formtoken"))
+	}
+	return strings.Contains(token, usertoken) && strings.Contains(token, r.URL.Path)
 }
 
 func GenerateToken(url string, usertoken string) string {
-	return Encrypt([]byte(FormKey), fmt.Sprintf("%s@%s@%s",url, time.Now().String() , usertoken) )
+	return Encrypt([]byte(FormKey), fmt.Sprintf("%s@%s@%s", url, time.Now().String(), usertoken))
 }
 
 func Encrypt(key []byte, text string) string {
@@ -176,10 +202,7 @@ func Encrypt(key []byte, text string) string {
 
 //func GetFile
 
-
 //func DeleteFile add support to html file filters : file:"mimeType"
-
-
 
 // decrypt from base64 to decrypted string
 func Decrypt(key []byte, cryptoText string) string {
